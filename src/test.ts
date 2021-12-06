f6ff1f47e081fedce26194dcbc40c2f2ae170088
René Schultz Madsen <rm@microting.com>
2021-12-06 15:46:08 +0000

    Making the tests work with angular 11.

--- src/test.ts
+++ src/test.ts
@@ -10,9 +10,13 @@ import {
 declare const require: any;
 
 // First, initialize the Angular testing environment.
+// getTestBed().initTestEnvironment(
+//   BrowserDynamicTestingModule,
+//   platformBrowserDynamicTesting()
+// );
 getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting()
+    BrowserDynamicTestingModule,
+    platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
